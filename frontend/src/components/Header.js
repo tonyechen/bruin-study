89a8f15b1bd6a17487f79b59b6c8f23a3d1144bd
Anthony Chen <88166446+tonyechen@users.noreply.github.com>
2022-04-29 10:53:49 -0700

    adjust to folder changes

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React from 'react';
+
+import './Header.css';
 
 // Header for the entire App
 const Header = () => {
-  return <div>Header</div>;
+    return (
+        <section className="header">
+            <li className="header_logo">Bruin Study</li>
+            <ul className='header_group'>
+                <li className='header_option'>Login</li>
+                <li className='header_option'>Logout</li>
+                <li className='header_option'>Profile Pics/DropDown</li>
+            </ul>
+        </section>
+    );
 };
 
 export default Header;
