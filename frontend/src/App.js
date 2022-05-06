b0c6d135ce5148a8b4130f058cdf2ed6566ca5ce
Raiun <ryan.man821@gmail.com>
2022-05-05 22:13:51 -0700

    Home Class Starter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-
 import Header from "./components/Header";
+import Home from "./components/Home";
 
 const App = () => {
   return <div>
       <Header />
+      <Home />
   </div>;
 };
 
---
+++ frontend/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Home {
+    render() {
+        return (
+            <div>
+                <h2>HOME</h2>
+            </div>
+        )
+    }
+}
+
+export default Home;
