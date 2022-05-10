40b9668d1a754fff8edd9a5de2ccecd339d58c00
vosima789 <vosima789@g.ucla.edu>
2022-05-09 20:39:23 -0700

    Update Solution.sql
    Another update to the schema to accommodate the group changing need.

--- backend/Solution.sql
+++ backend/Solution.sql
@@ -9,9 +9,12 @@ DROP TABLE IF EXISTS FAILED CASCADE;
 DROP TABLE IF EXISTS Messages CASCADE;
 DROP TABLE IF EXISTS Introduction CASCADE;
 **/
+CREATE TYPE MajorList AS ENUM =('Mathematics','Computer Science','Electrical Engineering','Physics');
 CREATE TABLE IF NOT EXISTS Student(
-	email varchar(40) PRIMARY KEY,
+	id numeric(9,0) PRIMARY KEY,
+	email varchar(40) UNIQUE NOT NULL,
 	Name varchar(100) NOT NULL,
+	major MajorList,
 	username varchar(30) UNIQUE NOT NULL,
 	password varchar(30) NOT NULL,
 	);
@@ -19,29 +22,29 @@ CREATE TABLE IF NOT EXISTS CourseList(
 	Abbreviation varchar(30) PRIMARY KEY
 	);
 CREATE TABLE IF NOT EXISTS Took(
-	email varchar(100) REFERENCES Student(email) ON UPDATE CASCADE,
+	id numeric(9,0) REFERENCES Student(id) ON UPDATE CASCADE,
 	course varchar(100) REFERENCES CourseList(Abbreviation) ON UPDATE CASCADE,
-	PRIMARY KEY(email, course)
+	PRIMARY KEY(id, course)
 	);
 CREATE TABLE IF NOT EXISTS Taking(
-	email varchar(100) REFERENCES Student(email) ON UPDATE CASCADE,
+	id numeric(9,0) REFERENCES Student(id) ON UPDATE CASCADE,
 	course varchar(100) REFERENCES CourseList(Abbreviation) ON UPDATE CASCADE,
-	PRIMARY KEY(email, course)
+	PRIMARY KEY(id, course)
 );
 CREATE TABLE IF NOT EXISTS FAILED(
-	email1 varchar(100) REFERENCES Student(email) ON UPDATE CASCADE,
-	email2 varchar(100) REFERENCES Student(email) ON UPDATE CASCADE,
-	PRIMARY KEY (email1, email2)
+	id1 numeric(9,0) REFERENCES Student(id) ON UPDATE CASCADE,
+	id2 numeric(9,0) REFERENCES Student(id) ON UPDATE CASCADE,
+	PRIMARY KEY (id1, id2)
 	);
 CREATE TABLE IF NOT EXISTS Messages(
-	email1 varchar(100) REFERENCES Student(email) ON UPDATE CASCADE,
-	email2 varchar(100) REFERENCES Student(email) ON UPDATE CASCADE,
+	id1 numeric(9,0) REFERENCES Student(id) ON UPDATE CASCADE,
+	id2 numeric(9,0) REFERENCES Student(id) ON UPDATE CASCADE,
 	content text,
 	stamp timestamptz,
-	PRIMARY KEY (email1,email2,stamp)
+	PRIMARY KEY (id1,id2,stamp)
 	);
 CREATE TABLE IF NOT EXISTS Introduction(
-	email varchar(100) REFERENCES Student(email) PRIMARY KEY,
+	id  numeric(9,0) REFERENCES Student(id) PRIMARY KEY,
 	Introduction text
 	);
 /**
