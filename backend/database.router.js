91b03c2911c2c63b243486804bd24a7c0fe29925
Anthony Chen <88166446+tonyechen@users.noreply.github.com>
2022-05-11 15:49:24 -0700

    Set up function templates for restAPI

--- backend/database.router.js
+++ backend/database.router.js
@@ -3,6 +3,6 @@ const api = require('./databaseDAO');
 
 const router = express.Router();
 
-router.route('/user/:uid').get(api.getStudentPofile);
+router.route('/user/:uid').get(api.getStudent);
 
 module.exports = router;
--- backend/databaseDAO.js
+++ backend/databaseDAO.js
@@ -5,74 +5,244 @@ const pool = require('./db');
 class API {
     // get student profile
     // input: id
-    static async getPofile(req, res) {
+    static async getStudent(req, res) {
         try {
             const { id } = req.params;
-            
+
             // Fetch all except password from student table
             // input: id
-            const student = await pool.query(
-                // ADD QUERY HERE, DELETE THIS COMMENT
-                
-            ).rows[0];
+            const student = await pool
+                .query
+                // ADD QUERY INSIDE PARENTHESIS, DELETE THIS COMMENT
+                ();
 
-            // Fetch all course taking by the student
-            // input: id
-            student.coursetaking = await pool.query(
-                // ADD QUERY HERE, DELETE THIS COMMENT
-            ).rows[0];
-
-            // Fetch all course taken by the student
-            // input: id
-            student.coursetaken = await pool.query (
-                // ADD QUERY HERE
-            ).rows[0];
-
-            res.json(student);
+            res.json(student.rows[0]);
         } catch (err) {
             console.error(err.message);
         }
     }
 
     // create student profile;
-    // input: id, email, Name, major, username, password, introduction
-    static async createProfile(req, res) {
+    // input: id, email, name, major, username, password
+    static async createStudent(req, res) {
         try {
-            const {} = req.query;
+            const { id, email, name, major, username, password } = req.query;
 
             // insert into Student
-            // input: id, email, Name, major, username, password
-            await pool.query(
-                // ADD QUERY HERE, DELETE THIS COMMENT
-            );
-
-            // insert into intro
-            // input: intro
-           await pool.query(
-                // ADD QUERY HERE, DELETE THIS COMMENT
-            );
+            // input: id, email, name, major, username, password
+            await pool
+                .query
+                // ADD QUERY INSIDE PARENTHESIS, DELETE THIS COMMENT
+                ();
         } catch (err) {
             console.error(err.message);
         }
     }
 
-    //updateProfile;
-    static async updateProfile(req, res) {
+    // update Student Profile;
+    // input: id, email, name, major, username, password
+    static async updateStudent(req, res) {
         try {
-            const {} = req.query;
+            const { id, email, name, major, username, password } = req.query;
 
+            // update existing student:
+            // input: id, email, name, major, username, password
+            await pool
+                .query
+                // ADD QUERY INSIDE PARENTHESIS, DELETE THIS COMMENT
+                ();
         } catch (err) {
             console.error(err.message);
         }
     }
-    //getCourseTook;
-    //addCourseTook;
-    //updateCourseTook;
-    //getCourseTaking;
-    //addCourseTaking;
-    //updateCourseTaking;
-    //addFailedMatches;
-    //getFailedMatches;
+
+    // retrieve all courses taken by student;
+    // input: id
+    static async getCourseTook(req, res) {
+        try {
+            const { id } = req.params;
+
+            // fetch all courses taken by a student
+            // input: id
+            const courseTook = await pool
+                .query
+                // ADD QUERY INSIDE PARENTHESIS, DELETE THIS COMMENT
+                ();
+
+            res.json(courseTook.rows[0]);
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
+    // add a course took by a student;
+    // input: id, course
+    static async addCourseTook(req, res) {
+        try {
+            const { id, course } = req.query;
+
+            // add a course taken by a student
+            // input: id, course
+            await pool
+                .query
+                // ADD QUERY INSIDE PARENTHESIS, DELETE THIS COMMENT
+                ();
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
+    // delete a Course Took by a student;
+    // input: id, course
+    static async deleteCourseTook(req, res) {
+        try {
+            const { id, course } = req.query;
+
+            // delete a course taken by a student
+            // input: id, course
+            await pool
+                .query // ADD QUERY INSIDE PARENTHESIS, DELETE THIS COMMENT
+                ();
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
+    // retrieve all courses student is taking;
+    // input: id
+    static async getCourseTaking(req, res) {
+        try {
+            const { id } = req.params;
+
+            // fetch all courses a student is taking
+            // input: id
+            const courseTaking = await pool
+                .query
+                // ADD QUERY INSIDE PARENTHESIS, DELETE THIS COMMENT
+                ();
+
+            res.json(courseTaking.rows[0]);
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
+    // add a course the student is taking;
+    // input: id, course
+    static async addCourseTaking(req, res) {
+        try {
+            const { id, course } = req.query;
+
+            // Add a course the student is taking
+            // input: id, course
+            await pool
+                .query
+                // ADD QUERY INSIDE PARENTHESIS, DELETE THIS COMMENT
+                ();
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
+    // delete a Course the student is taking;
+    // input: id, course
+    static async deleteCourseTaking(req, res) {
+        try {
+            const { id, course } = req.query;
+
+            // delete a course the student is taking
+            // input: id, course
+            await pool
+                .query // ADD QUERY INSIDE PARENTHESIS, DELETE THIS COMMENT
+                ();
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
+    // fetch all failed matches of the student
+    // input: id
+    static async getFailedMatch(req, res) {
+        try {
+            const { id } = req.params;
+
+            // get all failed matches of the student
+            // input: id
+            const failed = await pool
+                .query // ADD QUERY INSIDE PARENTHESIS, DELETE THIS COMMENT
+                ();
+
+            res.json(failed)
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
+    // insert a failed match of a student
+    // input: id1, id2
+    static async addFailedMatch(req, res) {
+        try {
+            const { id1, id2 } = req.query;
+
+            // Insert a failed match of the student(id1)
+            // input: id1, id2
+            await pool
+                .query // ADD QUERY INSIDE PARENTHESIS, DELETE THIS COMMENT
+                ();
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
+    // get the intro of a student
+    // input: id
+    static async getIntroduction(req, res) {
+        try {
+            const { id } = req.params;
+
+            // get the introudciont of the student
+            // input: id
+            const intro = pool
+                .query // ADD QUERY INSIDE PARENTHESIS, DELETE THIS COMMENT
+                ();
+
+            res.json(intro)
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
+    // update the intro of a student
+    // input: id, text
+    static async addIntroduction(req, res) {
+        try {
+            const { id, text } = req.query;
+
+            // add the introduction of the student
+            // input: id, text
+            const intro = pool
+                .query // ADD QUERY INSIDE PARENTHESIS, DELETE THIS COMMENT
+                ();
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
+    // update the intro of a student
+    // input: id, text
+    static async updateIntroduction(req, res) {
+        try {
+            const { id, text } = req.params;
+
+            // update the intro of the student
+            // input: id, text
+            const intro = pool
+                .query // ADD QUERY INSIDE PARENTHESIS, DELETE THIS COMMENT
+                ();
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
 }
 
 module.exports = API;
