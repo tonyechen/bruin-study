985035654e87d464931ee4d247f35b20a26ebc9c
Anthony Chen <88166446+tonyechen@users.noreply.github.com>
2022-05-11 10:51:25 -0700

    initialized function template for backend

--- backend/databaseDAO.js
+++ backend/databaseDAO.js
@@ -4,19 +4,75 @@ const pool = require('./db');
 
 class API {
     // get student profile
-    static async getStudentPofile(req, res) {
+    // input: id
+    static async getPofile(req, res) {
         try {
-            const { uid } = req.params;
+            const { id } = req.params;
+            
+            // Fetch all except password from student table
+            // input: id
             const student = await pool.query(
-                'SELECT * FROM students WHERE uid = ($1)',
-                [uid]
-            );
-            delete student.rows[0].password;
-            res.json(student.rows[0]);
+                // ADD QUERY HERE, DELETE THIS COMMENT
+                
+            ).rows[0];
+
+            // Fetch all course taking by the student
+            // input: id
+            student.coursetaking = await pool.query(
+                // ADD QUERY HERE, DELETE THIS COMMENT
+            ).rows[0];
+
+            // Fetch all course taken by the student
+            // input: id
+            student.coursetaken = await pool.query (
+                // ADD QUERY HERE
+            ).rows[0];
+
+            res.json(student);
         } catch (err) {
             console.error(err.message);
         }
     }
+
+    // create student profile;
+    // input: id, email, Name, major, username, password, introduction
+    static async createProfile(req, res) {
+        try {
+            const {} = req.query;
+
+            // insert into Student
+            // input: id, email, Name, major, username, password
+            await pool.query(
+                // ADD QUERY HERE, DELETE THIS COMMENT
+            );
+
+            // insert into intro
+            // input: intro
+           await pool.query(
+                // ADD QUERY HERE, DELETE THIS COMMENT
+            );
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
+    //updateProfile;
+    static async updateProfile(req, res) {
+        try {
+            const {} = req.query;
+
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+    //getCourseTook;
+    //addCourseTook;
+    //updateCourseTook;
+    //getCourseTaking;
+    //addCourseTaking;
+    //updateCourseTaking;
+    //addFailedMatches;
+    //getFailedMatches;
 }
 
 module.exports = API;
