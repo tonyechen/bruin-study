2aee95bc144534cd710f55a2da8c7f4f8f3141db
Aditya Mehta <adityam2350@gmail.com>
2022-05-12 15:32:10 -0700

    Completed full profile with added bio, name, ClassList, email fields

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -21,7 +21,7 @@ class ClassLine extends Component
                      name="ClassName" 
                      value={ClassName} 
                      onChange={this.handleChange}/>
-               <button onClick={()=>this.props.onDelete(this.props.id)}>
+               <button onClick={()=>this.props.onDelete(this.props.id)} type="button">
                    Delete Class
                </button>
             </div>
@@ -74,4 +74,79 @@ class ClassList extends Component
     }
 }
 
-export default ClassList;
+class editProfile extends Component
+{
+    state={
+        email: '',
+        bio:'',
+        classlist: [
+            {id: 1,  cn: ""},
+            {id: 2, cn: ""},
+            {id: 3, cn: ""},
+            {id: 4, cn: ""}
+        ]
+        
+    };
+    handleEmailChange = (e) =>
+    {
+        this.setState({email: e.target.value});
+    }
+    handleBioChange = (e) =>
+    {
+        this.setState({bio: e.target.value});
+    }
+    handleDelete=(whichID)=>
+    {
+        if(this.state.classlist.length>1)
+        {
+            let classlist=this.state.classlist.filter(c => c.id!== whichID);
+            for(var i=0; i<classlist.length;i++)
+            {
+                classlist[i].id=1+i;
+            }
+            this.setState({classlist: classlist});
+        }
+    }
+    handleAdd=()=>
+    {
+        if (this.state.classlist.length<10)
+        {
+            var x= this.state.classlist[this.state.classlist.length-1].id+1;
+            const classlist = this.state.classlist.concat({id: x, cn:""});
+            this.setState({classlist: classlist});
+        }
+    }
+    handleSubmit=()=>
+    {
+        alert(`${this.state.email} ${this.state.bio} ${this.state.classlist}`)
+    }
+    render()
+    {
+        const {ClassName}=this.state;
+        return(
+            
+            <div>
+            <h1>Edit Profile</h1>
+            <form onSubmit={this.handleSubmit}>
+                <div>
+                    <label>UID: 999999999</label>
+                    <br/>
+                    <label>Name: Aditya Mehta</label>
+                    <br/>
+                    <label>Email: </label>
+                    <input type="text" value={this.state.email} onChange={this.handleEmailChange}/>
+                    <br/>
+                    <label>Bio: </label>
+                    <textarea  value={this.state.bio} onChange={this.handleBioChange}/>
+                    {this.state.classlist.map(cl => <ClassLine key={cl.id} id={cl.id} onDelete={this.handleDelete} ClassName={cl.cn}>
+                    <label>Class {cl.id}: </label> </ClassLine>)}
+                    <button onClick={this.handleAdd} type="button">Add Another Class</button>
+                    <br/>
+                    <button type="submit">Submit </button>
+                </div>
+            </form>
+            </div>
+        );
+    }
+}
+export default editProfile;
