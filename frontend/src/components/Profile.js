feb7d192542a4a57f6d43df35875c38782791684
Aditya Mehta <adityam2350@gmail.com>
2022-05-13 11:07:29 -0700

    Added non-stylized error management on edit profile form

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,11 +1,12 @@
 import React, { Component, useState } from 'react'
+import * as EmailValidator from 'email-validator';
 import './Profile.css'
 
 
 class ClassLine extends Component
 {
     state = {
-        ClassName: this.props.ClassName
+        ClassName: this.props.ClassName,
     };
     handleChange = (e) =>
     {
@@ -13,80 +14,52 @@ class ClassLine extends Component
     }
     render()
     {
+        console.log(this.props.error)
         const {ClassName}=this.state;
         return(
             <div>
               {this.props.children}
               <input type="text" 
-                     name="ClassName" 
+                     name="ClassName"
+                     placeholder= "Class Name " 
                      value={ClassName} 
                      onChange={this.handleChange}/>
                <button onClick={()=>this.props.onDelete(this.props.id)} type="button">
                    Delete Class
                </button>
+               <label>{this.props.error}</label>
             </div>
         );
     }
 }
-class ClassList extends Component
-{
-    state={
-        classlist: [
-            {id: 1,  cn: ""},
-            {id: 2, cn: ""},
-            {id: 3, cn: ""},
-            {id: 4, cn: ""}
-        ]
-    };
-    handleDelete=(whichID)=>
-    {
-        if(this.state.classlist.length>1)
-        {
-            let classlist=this.state.classlist.filter(c => c.id!== whichID);
-            for(var i=0; i<classlist.length;i++)
-            {
-                classlist[i].id=1+i;
-            }
-            this.setState({classlist: classlist});
-        }
-    }
-    handleAdd=()=>
-    {
-        if (this.state.classlist.length<10)
-        {
-            var x= this.state.classlist[this.state.classlist.length-1].id+1;
-            const classlist = this.state.classlist.concat({id: x, cn:""});
-            this.setState({classlist: classlist});
-        }
-    }
 
-    render() {
-        return (
-        <div>
-            {this.state.classlist.map(cl => 
-            <ClassLine key={cl.id} id={cl.id} onDelete={this.handleDelete} ClassName={cl.cn}>
-                <label>Class {cl.id}: </label>
-            </ClassLine>
-            )}
-            <button onClick={this.handleAdd}>Add Another Class</button>
-        </div>
-        )
-    }
-}
+
+const initialState=
+{
+email: '',
+bio:'',
+emailError:'',
+bioError: '',
+CurrentClassList:[
+    {id: 1,  cn: "", error: ""},
+    {id: 2, cn: "", error: ""},
+    {id: 3, cn: "", error: ""},
+    {id: 4, cn: "", error: ""}
+],
+PreviousClassList: [
+    {id: 1,  cn: "", error: ""},
+    {id: 2, cn: "", error: ""},
+    {id: 3, cn: "", error: ""},
+    {id: 4, cn: "", error: ""}
+]
+    };
+
+
 
 class editProfile extends Component
 {
-    state={
-        email: '',
-        bio:'',
-        classlist: [
-            {id: 1,  cn: ""},
-            {id: 2, cn: ""},
-            {id: 3, cn: ""},
-            {id: 4, cn: ""}
-        ]
-        
-    };
+    state=initialState
+
     handleEmailChange = (e) =>
     {
         this.setState({email: e.target.value});
@@ -95,31 +68,99 @@ class editProfile extends Component
     {
         this.setState({bio: e.target.value});
     }
-    handleDelete=(whichID)=>
+    handleCurrentDelete=(whichID)=>
     {
-        if(this.state.classlist.length>1)
+        if(this.state.CurrentClassList.length>1)
         {
-            let classlist=this.state.classlist.filter(c => c.id!== whichID);
-            for(var i=0; i<classlist.length;i++)
+            let CurrentClassList=this.state.CurrentClassList.filter(c => c.id!== whichID);
+            for(var i=0; i<CurrentClassList.length;i++)
             {
-                classlist[i].id=1+i;
+                CurrentClassList[i].id=1+i;
             }
-            this.setState({classlist: classlist});
+            this.setState({CurrentClassList: CurrentClassList});
         }
     }
-    handleAdd=()=>
+    handleCurrentAdd=()=>
     {
-        if (this.state.classlist.length<10)
+        if (this.state.CurrentClassList.length<10)
         {
-            var x= this.state.classlist[this.state.classlist.length-1].id+1;
-            const classlist = this.state.classlist.concat({id: x, cn:""});
-            this.setState({classlist: classlist});
+            var x= this.state.CurrentClassList[this.state.CurrentClassList.length-1].id+1;
+            const CurrentClassList = this.state.CurrentClassList.concat({id: x, cn:"",error:""});
+            this.setState({CurrentClassList: CurrentClassList});
         }
     }
-    handleSubmit=()=>
+    handlePreviousDelete=(whichID)=>
     {
-        alert(`${this.state.email} ${this.state.bio} ${this.state.classlist}`)
+        if(this.state.PreviousClassList.length>1)
+        {
+            let PreviousClassList=this.state.PreviousClassList.filter(c => c.id!== whichID);
+            for(var i=0; i<PreviousClassList.length;i++)
+            {
+                PreviousClassList[i].id=1+i;
+            }
+            this.setState({PreviousClassList: PreviousClassList});
+        }
     }
+    handlePreviousAdd=()=>
+    {
+            var x= this.state.PreviousClassList[this.state.PreviousClassList.length-1].id+1;
+            const PreviousClassList = this.state.PreviousClassList.concat({id: x, cn:"", error:""});
+            this.setState({PreviousClassList: PreviousClassList});
+    }
+    validate = () => 
+    {
+        var check=true;
+        let bioError="";
+        if (this.state.bio==="")
+        {
+            bioError="Must fill in a bio";
+            check=false;
+        }
+        let emailError="";
+        if (this.state.email==="")
+        {
+            emailError="Must fill in email";
+            check=false;
+        }
+        else if(!EmailValidator.validate(this.state.email))
+        {
+            emailError="Must fill in a valid email";
+            check=false;
+        }
+
+
+        for(var i=0; i<this.state.PreviousClassList.length;i++)
+        {
+            if (this.state.PreviousClassList[i].cn=="")
+            {
+
+                this.state.PreviousClassList[i].error="Must fill in class name";
+                check=false;
+            }
+        }
+        for(var i=0; i<this.state.CurrentClassList.length;i++)
+        {
+            if (this.state.CurrentClassList[i].cn=="")
+            {
+                
+                this.state.CurrentClassList[i].error="Must fill in class name";
+                check=false;
+            }
+        }
+        this.setState({
+            emailError: emailError,
+            bioError: bioError,
+        });
+        return check;
+            
+        
+    }
+    handleSubmit= e =>
+    {
+        e.preventDefault();
+        const isValid=this.validate();
+        console.log(this.state);
+    };
     render()
     {
         const {ClassName}=this.state;
@@ -135,13 +176,24 @@ class editProfile extends Component
                     <br/>
                     <label>Email: </label>
                     <input type="text" value={this.state.email} onChange={this.handleEmailChange}/>
+                    <div>{this.state.emailError}</div>
                     <br/>
                     <label>Bio: </label>
                     <textarea  value={this.state.bio} onChange={this.handleBioChange}/>
-                    {this.state.classlist.map(cl => <ClassLine key={cl.id} id={cl.id} onDelete={this.handleDelete} ClassName={cl.cn}>
+                    <div>{this.state.bioError}</div>
+
+                    <dl>Current Classes</dl>
+                    {this.state.CurrentClassList.map(cl => <ClassLine key={cl.id} id={cl.id} onDelete={this.handleCurrentDelete} ClassName={cl.cn} error={cl.error}>
                     <label>Class {cl.id}: </label> </ClassLine>)}
-                    <button onClick={this.handleAdd} type="button">Add Another Class</button>
+                    <button onClick={this.handleCurrentAdd} type="button">Add Another Class</button>
                     <br/>
+
+                    <dl>Previous Classes</dl>   
+                    {this.state.PreviousClassList.map(cl => <ClassLine key={cl.id} id={cl.id} onDelete={this.handlePreviousDelete} ClassName={cl.cn} error={cl.error}>
+                    <label>Class {cl.id}: </label> </ClassLine>)}
+                    <button onClick={this.handlePreviousAdd} type="button">Add Another Class</button>
+                    <br/>
+
                     <button type="submit">Submit </button>
                 </div>
             </form>
