2d3c5e7b82a93d4f61b08804cf3aed845c358734
freed2003 <gary.song2003@gmail.com>
2022-05-14 08:23:19 -0700

    added login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 
 import Header from "./components/Header";
+import Login from "./components/Login";
 
 const App = () => {
   return <div>
-      <Header />
+      <Login />
   </div>;
 };
 
