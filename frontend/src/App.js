1b2c8839e22ede47821b9fde8ac2b1f166185079
freed2003 <gary.song2003@gmail.com>
2022-05-18 21:53:58 -0700

    header and app routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,22 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Login from "./components/Login";
-import Home from "./components/Home"
+import Home from "./components/Home";
+import Profile from "./components/Profile"
+import Signup from "./components/Signup"
 const App = () => {
   return ( 
     <Router>
       <div className="App">
-        <Header />
+        <div className="navbad">
+          <Header />
+        </div>
         <div className="content">
           <Routes>
             <Route exact path="/" element={<Home/>}/>
             <Route exact path="/login" element={<Login/>}/> 
+            <Route exact path="/signup" element={<Signup/>}/> 
+            <Route exact path="/profile" element={<Profile/>}/>
           </Routes>
         </div>
       </div>
