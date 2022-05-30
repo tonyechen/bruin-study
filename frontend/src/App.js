e8470d1eca023a00dc0b44ca36913a85f1d3736a
freed2003 <gary.song2003@gmail.com>
2022-05-29 19:03:45 -0700

    idk

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, useNavigate } from "react-router-dom";
 import Header from "./components/Header";
 import Login from "./components/Login";
 import Home from "./components/Home";
 import Profile from "./components/Profile"
 import Signup from "./components/Signup"
+
 const App = () => {
+  // const history = useNavigate();
   return ( 
     <Router>
       <div className="App">
