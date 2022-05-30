d2d68045886e8d0e6b6139fdefcc4135db2df8f9
Raiun <ryan.man821@gmail.com>
2022-05-29 17:08:55 -0700

    Pulling users from matching algorithm instead of local database + css updated

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
-import "./Home.css"
 import db from "../data/dataAccess";
+import getMatches from "../func/matching";
+import "./Home.css"
 
 class Home extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            uid: 111111111,
+            uid: 222222222,
             email: null,
             bio: null,
             username: null,
@@ -14,37 +15,30 @@ class Home extends React.Component {
             major: null,
             currentClassList: null,
             previousClassList: null,
-            yesSwp: [],
-            noSwp: null,
-            userIds: [111111111, 222222222, 333333333]
+            pmatches: [],
+            index: 1
         }
     }
 
     render() {
         return (
             <div className = "center">
-                <h1 className = "square">{this.displayStudent(this.state.uid)}</h1>,
+                <h1 className = "square">{this.displayStudent()}</h1>,
                 <button id = "No" onClick = {() => this.handleClick("No", this.state.uid)} className = "buttons">
                     No
                 </button>
-                <button id = "Rewind" onClick = {() => this.handleClick("Rewind", this.state.uid)} className = "buttons">
+                <button id = "Rewind" onClick = {() => this.handleClick("Rewind")} className = "buttons">
                     Rewind
                 </button>
-                <button id = "Yes" onClick = {() => this.handleClick("Yes", this.state.uid)} className = "buttons">
+                <button id = "Yes" onClick = {() => this.handleClick("Yes")} className = "buttons">
                     Yes
                 </button>
             </div>
         )
     }
 
-    displayStudent(id) {
-        //var ccl = [];
-        //var pcl = [];
-        /*for (var i = 0; i < this.state.CurrentClassList; i++) {
-            ccl.concat(this.state.CurrentClassList[i] + " | ");
-            console.log(ccl[i]);
-        }*/
-        if (this.state.uid !== 0) {
+    displayStudent() {
+        if (this.state.index !== -1 && this.state.index !== this.state.matches) {
             this.componentDidMount();
             return(
                 <h4 className = "profileBox">
@@ -65,32 +59,26 @@ class Home extends React.Component {
         }
     }
 
+    //Handles button clicks -> on Yes or no sends decisions to functions that update database information
     handleClick(buttonType, id) {
         console.log(buttonType);
-        if (buttonType === "Yes") {
-            //this.setState({yesSwp: yesSwp.push(id)})
-            //console.log(this.state.yesSwp);
+        if (buttonType === "Yes" && this.state.index !== -1) {
+            db.addMatch(this.state.uid, this.state.matches[this.state.index].id);
         }
         else if (buttonType === "Rewind") {
             //Set up pop up notification stuff
         }
-        else if (buttonType === "No") {
-            //this.setState({noSwp: [noSwp].concat(id)})
-            //console.log(this.state.noSwp);
+        else if (buttonType === "No" && this.state.index !== -1) {
+            //Failed match
         }
-        if (this.state.userIds[1]) {
-            this.state.userIds.splice(0, 1);
-            this.setState({
-                uid: this.state.userIds[0],
-            })
-        }
-        else {
-            this.setState({uid: 0})
-        }
+
+        this.setState({index: (this.state.matches[this.state.index + 1]) ? this.state.index + 1 : -1})
     }
 
     async componentDidMount() {
-        const obj = await db.getFullProfile(this.state.uid);
+        var matchesL = await getMatches(this.state.uid); 
+        const obj = await db.getFullProfile(matchesL[this.state.index].id);
+        
         var ccl = [];
         var pcl = obj.courseTook;
 
@@ -105,7 +93,8 @@ class Home extends React.Component {
                 name: obj.name,
                 major: obj.major,
                 currentClassList: ccl,
-                previousClassList: pcl
+                previousClassList: pcl,
+                matches: matchesL 
             }
         )
     }
