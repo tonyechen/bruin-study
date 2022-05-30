980c0fb46b844e43a310ceba1fb1044ea96add2c
Raiun <ryan.man821@gmail.com>
2022-05-29 18:57:56 -0700

    Finished Welcome Page, made rough About Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,12 @@ import Header from "./components/Header";
 //import Login from "./components/Login";
 import Home from "./components/Home";
 import About from "./components/About";
+import Welcome from "./components/Welcome"
 
 const App = () => {
   return <div>
       <Header />
-      <Home />
+      <Welcome />
   </div>;
 };
 
