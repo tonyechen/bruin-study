ac65f3f422de9cdba2141957c874229f5b23789d
Anthony Chen <88166446+tonyechen@users.noreply.github.com>
2022-05-30 17:32:48 -0700

    More CSS updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-d
 import Header from "./components/Header";
 import Login from "./components/Login";
 import Home from "./components/Home";
-import editfile from "./components/Profile"
+import EditProfile from "./components/Profile"
 import Profile from "./components/ProfPage"
 import Signup from "./components/Signup"
 import Welcome from "./components/Welcome"
@@ -13,22 +13,28 @@ import About from "./components/About"
 const App = () => {
   const shouldRedirect = true;
 
-  return ( 
-    <Router>
-      <div className="App">
-        <Header />
-          <Routes>
-            <Route path="/" element={shouldRedirect ? (<Navigate to="/welcome" />) : <Home />}/>
-            <Route path="/welcome" element={<Welcome/>}/>
-            <Route path="/home" element={<Home/>}/>
-            <Route path="/about" element={<About/>}/>
-            <Route path="/login" element={<Login/>}/> 
-            <Route path="/signup" element={<Signup/>}/> 
-            <Route path="/profile" element={<Profile/>}/>
-          </Routes>
-      </div>
-    </Router>
-  )
+  return (
+      <Router>
+          <div className="App">
+              <Header />
+              <Routes>
+                  <Route
+                      path="/"
+                      element={
+                          shouldRedirect ? <Navigate to="/welcome" /> : <Home />
+                      }
+                  />
+                  <Route path="/welcome" element={<Welcome />} />
+                  <Route path="/home" element={<Home />} />
+                  <Route path="/about" element={<About />} />
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/signup" element={<Signup />} />
+                  <Route path="/profile" element={<Profile />} />
+                  <Route path="/editProfile" element={<EditProfile />} />
+              </Routes>
+          </div>
+      </Router>
+  );
 };
 
 export default App;
