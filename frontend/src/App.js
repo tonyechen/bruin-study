59e79c0d9ec3eeeed68036b0203cd6db0f58981a
Anthony Chen <88166446+tonyechen@users.noreply.github.com>
2022-05-31 22:35:37 -0700

    added route editprofile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Login from './components/Login';
 import Home from './components/Home';
+import EditProfile from './components/Profile'
 import Profile from './components/ProfPage';
 import Signup from './components/Signup';
 import Welcome from './components/Welcome';
@@ -22,6 +23,7 @@ const App = () => {
                     <Route path="/login" element={<Login />} />
                     <Route path="/signup" element={<Signup />} />
                     <Route path="/profile" element={<Profile />} />
+                    <Route path="/editProfile" element={<EditProfile />} />
                 </Routes>
             </div>
         </Router>
